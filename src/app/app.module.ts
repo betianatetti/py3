60c716bd3ffab746fdcf9b97396f1b6f9b0463f3
Rocio <medinafrocio@gmail.com>
2019-08-07 19:30:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    NavbarComponent,
+    ReportesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
